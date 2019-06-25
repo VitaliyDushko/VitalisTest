cc786e52c22db24b2ca81fe19ebd33ff9d8c055e
vitaliy1507 <biller.driller.90@gmail.com>
2019-06-25 15:56:01 +0300

    Book store api migrations and db context

---
+++ BooksStore/Data/BookContext.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreUtilities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(b => b.RowVersion).IsConcurrencyToken();
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+    }
+}
---
+++ BooksStore/Migrations/20190619203752_RowVersion.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BooksStore.Migrations
+{
+    public partial class RowVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Book",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Book");
+        }
+    }
+}
